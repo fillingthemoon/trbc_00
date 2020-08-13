4724eb1e6d8fbdca3bc85706320a4021146f0430
philemonheng <phil.hsy@outlook.com>
2020-08-13 12:10:11 +0800

    Updates

---
+++ precache-manifest.d5be1d0d985e0f35cb34917351bac4b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbbc3237927a1977b7086ec8191d9fb",
+    "url": "/trbc_00/index.html"
+  },
+  {
+    "revision": "db750e037c439b6e2ef4",
+    "url": "/trbc_00/static/css/main.bd6aa8ba.chunk.css"
+  },
+  {
+    "revision": "15cff80771d191847455",
+    "url": "/trbc_00/static/js/2.d5b25d36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trbc_00/static/js/2.d5b25d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db750e037c439b6e2ef4",
+    "url": "/trbc_00/static/js/main.da8a186c.chunk.js"
+  },
+  {
+    "revision": "e34c7a1a1fe6995be6b6",
+    "url": "/trbc_00/static/js/runtime-main.185b0eb6.js"
+  },
+  {
+    "revision": "de45fdbdc5426ab5ed6fb37593979a63",
+    "url": "/trbc_00/static/media/exterior.de45fdbd.jpg"
+  },
+  {
+    "revision": "1d34ddf122e5b1064fe7862353f04578",
+    "url": "/trbc_00/static/media/logo.1d34ddf1.png"
+  },
+  {
+    "revision": "adc03bb98854c5c5412540aaf145d14d",
+    "url": "/trbc_00/static/media/trbc_map.adc03bb9.jpg"
+  }
+]);
