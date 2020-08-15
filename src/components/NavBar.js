2f7e1a7e8f7aee4ca80314367835b0b25be1ebad
philemonheng <phil.hsy@outlook.com>
2020-08-15 15:04:55 +0800

    fix nav-links vertical alignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,42 +4,38 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <nav className="navbar">
-      <Link to='/'>
-        <img
-          id="img-logo" 
-          src={require('../imgs/logo.png')} 
-          alt=""
-        />
+      <Link to="/">
+        <img id="img-logo" src={require('../imgs/logo.png')} alt="" />
       </Link>
       <ul className="main-nav" id="js-menu">
-        <Link to='/trbc_00/'>
+        <Link to="/trbc_00/">
           <li className="nav-links">Home</li>
         </Link>
-        <Link to='/trbc_00/aboutus'>
+        <Link to="/trbc_00/aboutus">
           <li className="nav-links">About Us</li>
         </Link>
-        <Link to='/trbc_00/joinus'>
+        <Link to="/trbc_00/joinus">
           <li className="nav-links">Join Us</li>
         </Link>
-        <Link to='/trbc_00/imnew'>
+        <Link to="/trbc_00/imnew">
           <li className="nav-links">I'm New</li>
         </Link>
-        <Link to='/trbc_00/outreach'>
+        <Link to="/trbc_00/outreach">
           <li className="nav-links">Outreach</li>
         </Link>
-        <Link to='/trbc_00/missions'>
+        <Link to="/trbc_00/missions">
           <li className="nav-links">Missions</li>
         </Link>
-        <Link to='/trbc_00/discipleship'>
+        <Link to="/trbc_00/discipleship">
           <li className="nav-links">Discipleship</li>
         </Link>
-        <Link to='/trbc_00/facilitybookings'>
+        <Link to="/trbc_00/facilitybookings">
           <li className="nav-links">Facility Bookings</li>
         </Link>
-        <Link to='/trbc_00/resources'>
+        <Link to="/trbc_00/resources">
           <li className="nav-links">Resources</li>
         </Link>
-        <Link>
+        <Link to="/trbc_00/">
           <li className="nav-links" id="lang-toggle">
             ENGLISH
           </li>
