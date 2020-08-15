42ea4cf8d66d1c027087198083fa3838d0c37222
philemonheng <phil.hsy@outlook.com>
2020-08-15 22:51:28 +0800

    edit navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,34 +8,34 @@ function NavBar() {
         <img id="img-logo" src={require('../imgs/logo.png')} alt="" />
       </Link>
       <ul className="main-nav" id="js-menu">
-        <Link to="/trbc_00/">
+        <Link to="/trbc_00/" className="main-nav-link">
           <li className="nav-links">Home</li>
         </Link>
-        <Link to="/trbc_00/aboutus">
+        <Link to="/trbc_00/aboutus" className="main-nav-link">
           <li className="nav-links">About Us</li>
         </Link>
-        <Link to="/trbc_00/joinus">
+        <Link to="/trbc_00/joinus" className="main-nav-link">
           <li className="nav-links">Join Us</li>
         </Link>
-        <Link to="/trbc_00/imnew">
+        <Link to="/trbc_00/imnew" className="main-nav-link">
           <li className="nav-links">I'm New</li>
         </Link>
-        <Link to="/trbc_00/outreach">
+        <Link to="/trbc_00/outreach" className="main-nav-link">
           <li className="nav-links">Outreach</li>
         </Link>
-        <Link to="/trbc_00/missions">
+        <Link to="/trbc_00/missions" className="main-nav-link">
           <li className="nav-links">Missions</li>
         </Link>
-        <Link to="/trbc_00/discipleship">
+        <Link to="/trbc_00/discipleship" className="main-nav-link">
           <li className="nav-links">Discipleship</li>
         </Link>
-        <Link to="/trbc_00/facilitybookings">
+        <Link to="/trbc_00/facilitybookings" className="main-nav-link">
           <li className="nav-links">Facility Bookings</li>
         </Link>
-        <Link to="/trbc_00/resources">
+        <Link to="/trbc_00/resources" className="main-nav-link">
           <li className="nav-links">Resources</li>
         </Link>
-        <Link to="/trbc_00/">
+        <Link to="/trbc_00/" className="main-nav-link">
           <li className="nav-links" id="lang-toggle">
             ENGLISH
           </li>
